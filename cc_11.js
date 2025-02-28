88b64d1e6f8296585dcdfb18d643532d37b41be0
mallikanicole <malmichael23@gmail.com>
2025-02-28 18:16:03 -0500

    Task 4- Implemente Book Borrowing

--- cc_11.js
+++ cc_11.js
@@ -56,20 +56,40 @@ console.log(borrower1.borrowedBooks);
 //Task 3 Creating a Library Class
 
 class Library {
-    constructor(books,borrowers) {
+    constructor() {
         this.books=[];
         this.borrowers=[];
     };//add books and borrowers to library class
+    addBorrower(borrower){this.borrowers.push(borrower)};
     addBook(book){
         this.books.push(book);
-    }//add books to library
+    };//add books to library
     listBooks(){
         this.books.forEach(book=>console.log(book.getDetails()));
     };//log details of books
+
+  //Task 4 Implementing Book Borrowing  
+    lendBook(borrowerID,isbn){
+        let book=this.books.find(book=>book.isbn===isbn);
+        if (book&&book.copies>0) {
+            let borrower=this.borrowers.find(borrower=>borrower.borrowerID===borrowerID);
+            if (borrower) {
+                borrower.borrowedBooks.push(book.title);
+                book.updateCopies(-1);
+            }
+        }
+    };
+    
 }
 
-//test
+//Test Task 3
 const library = new Library();
 library.addBook(book1);
 library.listBooks();
 // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+//Test Task 4
+library.lendBook(201, 123456);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 3"
+console.log(borrower1.borrowedBooks);
+// Expected output: ["The Great Gatsby"]
